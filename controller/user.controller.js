6e15810abd8128ab7676be3d81978eaf16eab94b
dimasliva <janzerikYT@yandex.ru>
2023-10-05 23:55:25 +0500

    node

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,7 +1,35 @@
 const db = require('../db')
 class UserController {
     async getUsers(req, res) {
-        const users = await db.query('SELECT * from users', (err, result) => {
+        await db.query('SELECT * from users', (err, result) => {
+            if(err) {
+                res.json(err)
+            } else {
+                res.json(result)
+            }
+        })
+    }
+    async getUser(req, res) {
+        const id = req.params.id
+        await db.query(`SELECT * from users where id = ${id}`, (err, result) => {
+            res.json(result)
+        })
+    }
+    async addUser(req, res) {
+        const {username, password} = req.body
+        await db.query(`INSERT INTO users(username, password) values (${username}, ${password})`, (err, result) => {
+            res.json(result)
+        })
+    }
+    async updateUser(req, res) {
+        const {id, username, password} = req.body
+        await db.query(`UPDATE users set username = ${username}, password = ${password}) where id = ${id}`, (err, result) => {
+            res.json(result)
+        })
+    }
+    async deleteUser(req, res) {
+        const id = req.params.id
+        await db.query(`DELETE from users where id = ${id}`, (err, result) => {
             res.json(result)
         })
     }
--- routes/user.router.js
+++ routes/user.router.js
@@ -3,4 +3,8 @@ const router = new Router()
 const userController = require('../controller/user.controller')
 
 router.get('/users', userController.getUsers);
+router.get('/users/:id', userController.getUser);
+router.put('/users/:id', userController.updateUser);
+router.post('/users/', userController.addUser);
+router.delete('/users/', userController.deleteUser);
 module.exports = router

18b3535aa2d69e63bd953d9c52b46d6749e2a92c
dimasliva <janzerikYT@yandex.ru>
2023-10-07 00:38:38 +0500

    add tasks

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const userRouter = require('./routes/user.router')
 const PORT = 8080
 const app = express()
 
+const cors=require("cors");
+const corsOptions ={
+    origin:'*',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+}
+
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 app.use(express.json())
 app.use('/api', userRouter);
+
 app.listen(PORT, () => console.log('http://localhost:8080/'))

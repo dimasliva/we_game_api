0fdae928b03d4f4ad02a266ae7187fb2b2cbf3e8
dimasliva <janzerikYT@yandex.ru>
2023-10-09 08:52:43 +0500

    add column items

--- controller/item.controller.js
+++ controller/item.controller.js
@@ -23,8 +23,8 @@ class ItemController {
         if(!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()})
         }
-        const {name} = req.body
-        const sql = `INSERT INTO items(name) values ('${name}')`
+        const {name, description} = req.body
+        const sql = `INSERT INTO items(name, description, category) values ('${name}', '${description}', '${category}')`
 
         await db.query(sql, (err, result) => {
             console.log(sql)
@@ -36,10 +36,9 @@ class ItemController {
         if(!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()})
         }
-        const {id, name} = req.body
-        const sql = `UPDATE items set name = '${name}' where id=${id}`
+        const {id, name, description, category} = req.body
+        const sql = `UPDATE items set name = '${name}', description='${description}', category='${category}' where id=${id}`
         await db.query(sql, (err, result) => {
-            console.log('sql', sql)
             console.log('result', result)
             res.json(result)
         })
--- routes/item.router.js
+++ routes/item.router.js
@@ -7,10 +7,14 @@ router.get('/items/', itemController.getItems);
 router.get('/items/:id', itemController.getItem);
 router.put('/items/', [
     body('id').trim().notEmpty(),
-    body('name').trim().notEmpty()
+    body('name').trim().notEmpty(),
+    body('description').trim().notEmpty(),
+    body('category').trim().notEmpty()
 ], itemController.updateItem);
 router.post('/items/', [
-    body('name').trim().notEmpty().isLength({min: 3, max: 30})
+    body('name').trim().notEmpty().isLength({min: 3, max: 30}),
+    body('description').trim().notEmpty().isLength({min: 3, max: 100}),
+    body('category').trim().notEmpty().isLength({min: 3, max: 30})
 ],itemController.addItem);
 router.delete('/items/:id', itemController.deleteItem);
 module.exports = router

258f310dadaec12d93bdc35e57c259a7befbf420
dimasliva <janzerikYT@yandex.ru>
2023-10-12 20:08:20 +0500

    change db

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 const conn = mysql.createConnection({
-    host: "sql8.freesqldatabase.com",
-    user: "sql8651326",
-    password: "n6xAyLB5T6",
-    port: "3306",
-    database: "sql8651326"
+    host: "dpg-ckf5qsmafg7c73fvd0mg-a.frankfurt-postgres.render.com/laravel_wwrp",
+    user: "laravel_wwrp_user",
+    password: "i91QQQpISJ8qnshaUUp0C33xXapQS7fn",
+    port: "5432",
+    database: "laravel_wwrp"
 })
 
 module.exports = conn
